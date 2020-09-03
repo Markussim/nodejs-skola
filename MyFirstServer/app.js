c34be5cb38d3292c41e69099d0592298a1b94f11
Markussim <you@example.com>
2020-09-03 12:51:49 +0200

    Did school work

--- MyFirstServer/app.js
+++ MyFirstServer/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const clientdir = __dirname + "/client"
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.sendFile(clientdir + "/index.html"))
+app.get('/style', (req, res) => res.sendFile(clientdir + "/style.css"))
+app.get('/image', (req, res) => res.sendFile(clientdir + "/G9bDaPH.jpg"))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
