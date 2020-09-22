e26fcd49bd93d8536aa54f1d4fcba3e0c8101ecc
Markussim <markus.simonsen@gmail.com>
2020-09-22 10:40:38 +0200

    Added mongodb

--- typescriptSkola/src/index.ts
+++ typescriptSkola/src/index.ts
@@ -1,1 +1,34 @@
-console.log("Hmm")
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log("Connected")
+});
+
+const kittySchema = new mongoose.Schema({
+    name: String
+});
+
+kittySchema.methods.speak = function () {
+    const greeting = this.name
+        ? "Meow name is " + this.name
+        : "I don't have a name";
+    console.log(greeting);
+}
+
+const Kitten = mongoose.model('Kitten', kittySchema);
+
+
+
+for (let i = 0; i < 100; i++) {
+    const fluffy = new Kitten({ name: 'fluffy' + i });
+
+    fluffy.save(function (err: Error, fluffy: any) {
+        if (err) return console.error(err);
+        fluffy.speak();
+    });
+}
